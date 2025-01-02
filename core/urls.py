cffa3fb42a63e0c8662042db59bd67f078a662d1
MohammadAli <hosseinkhosroabadi45@gmail.com>
2025-01-02 15:41:23 +0330

    [Fix] url

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("jwt/create/", CustomObtainPairView.as_view(), name="customtoken"),
     path('register/', UserRegisterView.as_view(), name='user-register'),
     path('verify/<int:user_id>/', VerifyUserApiView.as_view(), name='user-verify'),
- 
+    path("profile/", UserProfileDetail.as_view(), name="user-profile"),
 
 
 ]
